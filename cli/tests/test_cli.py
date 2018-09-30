25a069bf5ae5ef28f4203045cff8e0185e9d5035
dehora <bill@dehora.net>
2018-09-30 15:13:57 +0100

    Updates test for click 7.

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -7,7 +7,7 @@ def test_entrypoint():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(mli, [], catch_exceptions=False)
-        out = result.output_bytes.decode('utf-8')
+        out = result.output
         assert "Usage: mli [OPTIONS] COMMAND [ARGS]" in out
         assert "Options:" in out
         assert "  --help  Show this message and exit." in out
